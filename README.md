db893e2ee205910a310172f78ba1eb2d2e1e134f
Humoyun <h.ahmedov209@gmail.com>
2023-12-02 23:27:18 +0500

    💬 Welcome to DB

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 ### KinoPoisk API Bot
 
-**The application for training with asynchronous python libraries**
+**The application for training with asynchronous python libraries**<br>
 I created a Telegram bot to search for movies from Kinopoisk, this is my educational project to provide the capabilities of asynchronous Python libraries.
 
 Using the bot, you can filter or search for a movie. There is also the option to use ChatGPT
 
-__For these purposes I used libraries such as:__
+**For these purposes I used libraries such as:**
 - Aiohttp (For receiving data from API)
 - Aiosqlite (For working with a database)
 - Aiofiles (For working with files)
--- db/database.py
+++ db/database.py
@@ -1,14 +1,27 @@
+import datetime
+from sqlite3 import ProgrammingError
+
 import aiosqlite
+from datetime import datetime, date,
 
 
 class DataBase:
     def __init__(self, name) -> None:
         self.name = name
+
+    @staticmethod
+    def get_date(to_time: str | None = None) -> str | datetime:
+        format_date = '%Y-%m-%d %H:%M:%S'
+        if to_time is not None:
+            return datetime.strptime(to_time, format_date)
+        now = datetime.now()
+        now_str = now.strftime(format_date)
+        return now_str
     
     async def create_user(self, user_id, username):
         async with aiosqlite.connect(self.name) as db:
             if not await self.get_me(user_id):
-                await db.execute('INSERT INTO users(user_id, username) VALUES (?, ?)', (user_id, username))
+                await db.execute('INSERT INTO users(user_id, username, joined, last_time) VALUES (?, ?)', (user_id, username))
                 await db.commit()
             else:
                 pass
@@ -17,4 +30,13 @@ class DataBase:
         async with aiosqlite.connect(self.name) as db:
             result = await db.execute('SELECT * FROM users WHERE user_id = ?', (user_id, ))
             return await result.fetchone()
+
+    async def save_query(self, user_id, query):
+        async with aiosqlite.connect(self.name) as db:
+            now = self.get_date()
+            try:
+                await db.execute('INSERT INTO history_queries(query, created, user_id) VALUES(?, ?, ?)', (query, user_id, now))
+            except ProgrammingError as e:
+                pass
+
 
